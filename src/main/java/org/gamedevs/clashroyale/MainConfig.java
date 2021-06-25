79c969c6ed29da91ad4011c7f2228ec560c2d072
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-06-25 17:27:43 +0430

    Adding MainConfig inteface to set a application prebuild configurations

---
+++ src/main/java/org/gamedevs/clashroyale/MainConfig.java
@@ -0,0 +1,20 @@
+package main.java.org.gamedevs.clashroyale;
+
+/**
+ * This interface contains main configuration values
+ * of ClashRoyale application!
+ * @author Pouya Mohammadi - "pouyamohammadyirbu@gmail.com"
+ * @version 1.0 'Developing mode'
+ */
+public interface MainConfig {
+
+    /**
+     * If you want to enable methods and features
+     * that helps you to test and trace application
+     * performance set its value to 'true'.
+     * Else if you want to release the product,
+     * set this value to 'false'.
+     */
+    boolean DEBUG_MODE = true;
+
+}
