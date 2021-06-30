cf610d3458cb633909fb23f84b638d3e8a28684e
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-07-01 02:53:29 +0430

    feat: adding battle menu popup bringer

---
+++ src/main/java/org/gamedevs/clashroyale/controller/menu/main/BattlePopup.java
@@ -0,0 +1,19 @@
+package org.gamedevs.clashroyale.controller.menu.main;
+
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.beans.EventHandler;
+
+public class BattlePopup {
+
+    @FXML
+    private Button cancelBtn;
+
+    @FXML
+    public void cancel(Event Event){
+
+    }
+
+}
--- src/main/java/org/gamedevs/clashroyale/controller/menu/main/MainBattle.java
+++ src/main/java/org/gamedevs/clashroyale/controller/menu/main/MainBattle.java
@@ -1,4 +1,48 @@
 package org.gamedevs.clashroyale.controller.menu.main;
 
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import org.gamedevs.clashroyale.model.utils.console.Console;
+
 public class MainBattle {
+
+    @FXML
+    private Button battleBtn;
+
+    private final ImageView backgroundCover = new ImageView(new Image(
+            getClass().getResource("./../../../view/img/menu/menu_background_cover_v2.png").toExternalForm()));
+
+    @FXML
+    private void bringBattlePopup(Event event){
+        try{
+            Group popupRoot = new Group();
+            AnchorPane popupMenu = FXMLLoader.load(getClass().getResource(
+                    "./../../../view/fxml/menu/battle_menu_popup.fxml"
+            ));
+            popupRoot.getChildren().add(backgroundCover);
+            popupMenu.setTranslateX(48.5);
+            popupMenu.setTranslateY(202.5);
+            popupRoot.getChildren().add(popupMenu);
+//            Scene scene = new Scene(popupMenu);
+//            Stage stage = new Stage();
+            Scene mainBattleMenuScene = battleBtn.getScene();
+            AnchorPane mainBattleMenu = (AnchorPane) mainBattleMenuScene.getRoot();
+            mainBattleMenu.getChildren().add(popupRoot);
+
+//            stage.setScene(scene);
+//            stage.show();
+        }catch (Exception e){
+            Console.getConsole().printTracingMessage("Failed to pop up battle menu");
+        }
+    }
+
 }
