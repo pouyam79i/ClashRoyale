bb98c4f500f2d0505dfe02abaaccad2b0259bbb0
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-07-11 15:12:22 +0430

    feat: creating all card class but not coded

---
+++ src/main/java/org/gamedevs/clashroyale/model/cards/soldier/BabyDragon.java
@@ -0,0 +1,4 @@
+package org.gamedevs.clashroyale.model.cards.soldier;
+
+public class BabyDragon {
+}
---
+++ src/main/java/org/gamedevs/clashroyale/model/cards/spell/Arrows.java
@@ -0,0 +1,4 @@
+package org.gamedevs.clashroyale.model.cards.spell;
+
+public class Arrows {
+}
