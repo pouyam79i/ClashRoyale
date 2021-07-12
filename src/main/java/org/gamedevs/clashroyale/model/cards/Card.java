0089002100cca31000f849cb108eda0e8c12c290
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-07-12 04:47:47 +0430

    fix: removing useless field from card model

--- src/main/java/org/gamedevs/clashroyale/model/cards/Card.java
+++ src/main/java/org/gamedevs/clashroyale/model/cards/Card.java
@@ -20,18 +20,6 @@ public class Card implements Serializable {
      * Cost of card (elixir)
      */
     private final int cost;
-    /**
-     * Current xp of card
-     */
-    protected int xp;
-    /**
-     * Current leve of card
-     */
-    protected int leve;
-    /**
-     * Xp needed to upgrade card!
-     */
-    protected int levelUpXPScale;
 
     /**
      * Constructor of Card
@@ -42,8 +30,6 @@ public class Card implements Serializable {
     public Card(CardName cardName, int cost) {
         this.cardName = cardName;
         this.cost = cost;
-        xp = 0;
-        leve = 1;
     }
 
     // Getters
